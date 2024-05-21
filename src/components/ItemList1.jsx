1c75aae8e87c16dad729c373cbd149aeca355909
pawan3216kumar <dhananjay965114@gmail.com>
2024-05-21 15:55:57 +0530

    first commit

---
+++ src/components/ItemList1.jsx
@@ -0,0 +1,31 @@
+// src/components/ItemList1.jsx
+import { useState } from "react";
+
+export default function ItemList1() {
+  const [items, setItems] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+
+  const addItem = () => {
+    if (inputValue.trim()) {
+      setItems([...items, inputValue]);
+      setInputValue("");
+      console.log(inputValue)
+    }
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <button onClick={addItem}>Add Item</button>
+      <ul>
+        {items.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
---
+++ src/components/TaskManager.jsx
@@ -0,0 +1,38 @@
+// src/components/TaskManager.jsx
+
+import { useState } from "react";
+
+function TaskManager() {
+  const [tasks, setTasks] = useState([]);
+  const [task, setTask] = useState("");
+
+  const addTask = () => {
+    if (task.trim()) {
+      setTasks([...tasks, task]);
+      tasks("");
+    }
+  };
+
+  const removeTask = () => {
+    setTasks(tasks.slice(1)); 
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+      />
+      <button onClick={addTask}>Add Task</button>
+      <button onClick = {removeTask}>Remove task</button>
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index}>{task}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default TaskManager;
